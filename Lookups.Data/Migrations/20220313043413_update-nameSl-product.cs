767bf1c73d88a1c7e91ca7cb3c704b9a815f8b22
Anwar <Anwar@DESKTOP-0OH6CLM>
2022-03-13 06:37:40 +0200

    add name sl

---
+++ Lookups.Data/Migrations/20220313043413_update-nameSl-product.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Orders.Data.Migrations
+{
+    public partial class updatenameSlproduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000003"),
+                column: "NameSl",
+                value: "Kook Pop");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000004"),
+                column: "NameSl",
+                value: "�unatort");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000005"),
+                column: "NameSl",
+                value: "Vesi");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000001"),
+                column: "NameSl",
+                value: "S�rk");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000002"),
+                column: "NameSl",
+                value: "P�ksid");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000003"),
+                column: "NameSl",
+                value: "Jope");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000004"),
+                column: "NameSl",
+                value: "M�nguasi");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000003"),
+                column: "NameSl",
+                value: "Cake Pop");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000004"),
+                column: "NameSl",
+                value: "Apple Tart");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000005"),
+                column: "NameSl",
+                value: "Water");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000001"),
+                column: "NameSl",
+                value: "Shirt");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000002"),
+                column: "NameSl",
+                value: "Pants");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000003"),
+                column: "NameSl",
+                value: "Jacket");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000004"),
+                column: "NameSl",
+                value: "Toy");
+        }
+    }
+}
